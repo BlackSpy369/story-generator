e5e6355b61502718c817fe276b04f191dc377db7
Vansh Sharma <blackspyyt369@gmail.com>
2024-06-07 09:57:36 +0000

    Added sidebar and some improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,25 @@ if "model" not in st.session_state:
 
 st.title("STORY GENERATION APP")
 
-prompt=st.text_input("Enter story first line")
-if prompt!="":
-   st.write(st.session_state.model(prompt,max_length=200)[0]["generated_text"])
+with st.status("Status",expanded=False) as status:
+    st.write("Loading Model...")
+
+with st.sidebar:
+    max_length=st.slider("Max Length",10,800,200,10)
+
+# prompt=st.text_input("Enter first line of story (min 5 words)")
+prompt=st.text_area("Enter first line of story (min 5 words)")
+if st.button("Generate"):
+    if len(prompt.split(" "))>5:
+        with st.container(border=True):
+            status.write("Generating Story, Please wait...")
+            status.update(label="Status",expanded=True,state="running")
+            st.write(st.session_state.model(prompt,max_length=max_length)[0]["generated_text"])
+            status.update(label="Status",expanded=False,state="complete")
+            status.write("Completed...")
+
+    else:
+        status.update(label="Status",expanded=False,state="error")
+        st.error("Please atleast enter five words of story...")
+
+st.info("Sometimes Model may be unavailbale to generate, it will end up repeating words, in that case please reduce max length to 100 to 200 or try to regenerate...")
