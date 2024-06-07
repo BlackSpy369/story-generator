5b73a1187898b3e802e8657ce7293ef1fa6a8681
Vansh Sharma <blackspyyt369@gmail.com>
2024-06-07 03:33:10 +0000

    basic app builded

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,16 @@ from transformers import pipeline
 
 st.set_page_config(page_title="Story Generation",page_icon="🤗")
 
+if "model" not in st.session_state:
+    from transformers import AutoModelForCausalLM, AutoTokenizer
 
+    # Load the base model and tokenizer
+    base_model = AutoModelForCausalLM.from_pretrained("fine_tuned_model")
+    tokenizer = AutoTokenizer.from_pretrained("fine_tuned_model")
+    st.session_state.model=pipeline("text-generation",model=base_model,tokenizer=tokenizer)
+
+st.title("STORY GENERATION APP")
+
+prompt=st.text_input("Enter story first line")
+if prompt!="":
+   st.write(st.session_state.model(prompt,max_length=200)[0]["generated_text"])
